9f9e24d960e3a6e35d6fd189a930b70ab9bdc621
nst2000 <sennoel2000@outlook.co.nz>
2021-09-19 23:53:51 +1200

    Material Table Sorting - sample table
    Added a few more claims
    
    Co-Authored-By: shmackedUP <81208908+shmackedUP@users.noreply.github.com>

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -1,63 +1,116 @@
 import React from 'react'
 import MaterialTable from 'material-table'
 
-export const Table=()=>{
-    const data=[
-        {title: 'An experimental evaluation of test driven development vs. test-last development with industry professionals',
-        authors: "Munir, H., Wnuk, K., Petersen, K., Moayyed, M.",
-        source: "EASE",
-        pubyear: "2014",
-        doi: "https://doi.org/10.1145/2601248.2601267",
-        claim: "product quality improvement", 
-        evidence: "weak support"},
-        
-        {title: 'Realizing quality improvement through test driven development: results and experiences of four industrial teams',
-        authors: "Nagappan, N., Maximilien, E. M., Bhat, T., Williams, L.",
-        source: " Empirical Software Engineering, 13(3), 289–302",
-        pubyear: "2008",
-        doi: "https://doi.org/10.1007/s10664-008-9062-z",
-        claim: "product quality improvement", 
-        evidence: "weak support"},
 
-        {title: "Does Test-Driven Development Really Improve Software Design Quality?",
-        authors: "Janzen, D. S.",
-        source: "Software, IEEE, 25(2) 77-84",
-        pubyear: "2008",
-        doi: "",
-        claim: "code quality improvement", 
-        evidence: "strong support"},
-        
+export const Table = () => {
+    const data = [{
+            title: 'An experimental evaluation of test driven development vs. test-last development with industry professionals',
+            authors: "Munir, H., Wnuk, K., Petersen, K., Moayyed, M.",
+            source: "EASE",
+            pubyear: "2014",
+            doi: "https://doi.org/10.1145/2601248.2601267",
+            claim: "product quality improvement",
+            evidence: "weak support"
+
+        },
+
+        {
+            title: 'Realizing quality improvement through test driven development: results and experiences of four industrial teams',
+            authors: "Nagappan, N., Maximilien, E. M., Bhat, T., Williams, L.",
+            source: " Empirical Software Engineering, 13(3), 289–302",
+            pubyear: "2008",
+            doi: "https://doi.org/10.1007/s10664-008-9062-z",
+            claim: "product quality improvement",
+            evidence: "weak support"
+        },
+
+        {
+            title: "Does Test-Driven Development Really Improve Software Design Quality?",
+            authors: "Janzen, D. S.",
+            source: "Software, IEEE, 25(2) 77-84",
+            pubyear: "2008",
+            doi: "",
+            claim: "code quality improvement",
+            evidence: "strong support"
+        },
+
+        {
+            title: "Comparative Case Study on the Impact of Test-Driven Development on Program Design and Test Coverage",
+            authors: "Siniaalto, M., Abrahamsson, P.",
+            source: "ArXiv.Org, cs.SE, arXiv:1711.05082-284",
+            pubyear: "2017",
+            doi: "",
+            claim: "code quality improvement",
+            evidence: "weak support"
+        },
+
+        {
+            title: "Driving Software Quality: How Test-Driven Development Impacts Software Quality.",
+            authors: "Crispin, L",
+            source: "IEEE Software, 23(6), 70–71.",
+            pubyear: "2006",
+            doi: "https://doi.org/10.1109/ms.2006.157",
+            claim: "code quality improvement",
+            evidence: "strong support"
+        },
+
+        {
+            title: "Test-Driven Development as a Defect-Reduction Practice.",
+            authors: "Williams, L., Maximilien, E. M. & Vouk, M.",
+            source: "4th International Symposium on Software Reliability Engineering, 2003. ISSRE 2003, 34–45.",
+            pubyear: "2001",
+            doi: "https://doi.org/10.1109/issre.2003.1251029",
+            claim: "Product Quality Improvement",
+            evidence: "strong support"
+        },
+
+        {
+            title: "An Empirical Study of Test-Driven Development vs. Test-Last Development Using Eye Tracking.",
+            authors: "Choma, J., Guerra, E. M., Silva, T. S. da, Albuquerque, T., Albuquerque, V. G. & Zaina, L. M.",
+            source: "WBMA, 1106(1), 11–24.",
+            pubyear: "2019",
+            doi: "https://doi.org/10.1007/978-3-030-36701-5_2",
+            claim: "code quality improvement",
+            evidence: "weak support"
+        },
     ]
 
-    const columns=[
-        {
-            title: 'Title', field: 'title'
+    const columns = [{
+            title: 'Title',
+            field: 'title'
         },
         {
-            title: 'Authors', field: 'authors'
+            title: 'Authors',
+            field: 'authors'
         },
         {
-            title: 'Source', field: 'source'
+            title: 'Source',
+            field: 'source'
         },
         {
-            title: 'Pubyear', field: 'pubyear'
+            title: 'Pubyear',
+            field: 'pubyear'
         },
         {
-            title: 'DOI', field: 'doi'
+            title: 'DOI',
+            field: 'doi'
         },
         {
-            title: 'Claim', field: 'claim'
+            title: 'Claim',
+            field: 'claim'
         },
         {
-            title: 'Evidence', field: 'evidence'
+            title: 'Evidence',
+            field: 'evidence'
         }
 
     ]
 
-    return(<div>
-        <MaterialTable title="Material Table"
-        data={data}
-        columns={columns}
-    />
-    </div>)
+    return ( < div >
+        <
+        MaterialTable title = "Material Table"
+        data = { data }
+        columns = { columns }
+        /> </div > )
+
 }
