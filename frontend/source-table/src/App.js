56a9ac3bbbcefe6def9d523950148faa57af097f
chrislau0304 <christopherlau16@gmail.com>
2021-09-19 19:59:21 +1200

    created a base table using material table

---
+++ frontend/source-table/src/App.js
@@ -0,0 +1,15 @@
+import { AddBox, ArrowDownward } from "@material-ui/icons";
+import logo from './logo.svg';
+import './App.css';
+import { Table } from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React-App</h2>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
