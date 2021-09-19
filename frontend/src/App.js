46439c8d73e270c465d2821e34bdf5baf6833de3
nst2000 <sennoel2000@outlook.co.nz>
2021-09-19 22:49:11 +1200

    Material Table Sorting - sample table
    all the relevant columns such as name and dates can be ordered in ascending or descending order depending on alphabetical or numerical standards
    
    Co-Authored-By: shmackedUP <81208908+shmackedUP@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 import { Table } from './Table';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>React-App</h2>
-      <Table/>
-    </div>
-  );
+    return ( < div className = "App" >
+        <
+        h2 > React - App < /h2> <Table / >
+        <
+        /div>
+    );
 }
 
 export default App;
