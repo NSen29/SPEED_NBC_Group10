3d0ddc054674eda2ac0feb38d13ca21ce5614f38
chrislau0304 <christopherlau16@gmail.com>
2021-09-21 20:44:00 +1200

    added searching filtering sorting features to table

--- frontend/source-table/src/App.js
+++ frontend/source-table/src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 import { Table } from './Table';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>React-App</h2>
-      <Table/>
-    </div>
-  );
+
+    const columns = [
+
+    ]
+
+    return ( <
+        div className = "App" >
+        <
+        h2 > < /h2> <
+        Table / >
+        <
+        /div>
+    );
 }
 
 export default App;
