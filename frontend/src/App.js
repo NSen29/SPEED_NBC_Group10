a3f74810859c388aa917d1d6d146a9f2c2322186
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-09-24 11:14:33 +1200

    second commit, deleted some stuff, added some stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,13 @@
 import { AddBox, ArrowDownward } from "@material-ui/icons";
-
-
 import { Table } from './Table';
 
-
 function App() {
-
-    return ( < div className = "App" >
-        <
-        h2 > < /h2> <Table / >
-        <
-
-
-        /div>
-        
-    );
+  return (
+    <div className="App">
+      <h2>React-App</h2>
+      <Table/>
+    </div>
+  );
 }
 
 export default App;
