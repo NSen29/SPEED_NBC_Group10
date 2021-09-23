0a8b81f452f2c17cff65935c1236eccab0bcf21e
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-09-23 19:36:14 +1200

    added heroku scripts for deployment, added server.js code

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const app = express()
 
 app.listen(PORT, console.log("Server running on port ${PORT}"));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
 }
