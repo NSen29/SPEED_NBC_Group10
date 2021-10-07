d9d84e9b638183b90a372bf09b10557e9ec2c9d2
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-10-08 12:39:18 +1300

    added basic dropdown, need to add routing

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
-
+import Dropdown from "../components/Dropdown.js";
 
 
 const SEPractice = () => {
-    return ( <
-        div >
-        <
-        h2 className = 'sm:text-4xl text-2xl font-bold mt-6 text-gray-900' > Select SE Practice to get evidence
-        for the claimed benefits < /h2>  <
-        ul >
-        <
-        li className = "inline-block pt-4 pb-4" >
-        <
-        Link to = "/TDD"
-        className = "pl-6 pr-8" >
-        Test Driven Development <
-        /Link>  < /
-        li > <
-        /ul>   < /
-        div >
+    return ( 
+
+
+        < div >
+        <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+        <Dropdown/>
+         <optionItems tag={Link} to="/TDD"></optionItems>
+           
+            
+
+        </div>
+    
+
 
     );
 }
