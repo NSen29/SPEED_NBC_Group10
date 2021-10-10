8290bed016f9e709f8423572b2692ccd64660baf
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-10-10 16:58:59 +1300

    added unit tests for submission form

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,15 +1,34 @@
 import React from 'react';
     import ReactDOM from 'react-dom';
     import App from './App';
-    
-
-    test('fake test', () => {
-      expect(true).toBeTruthy();
-    });
-
+    import CreateArticle from './pages/Submit-Article';
+    import { render } from '@testing-library/react';
 
 
     it('renders without crashing', () => {
       const div = document.createElement('div');
       ReactDOM.render(<App />, div);
+    });
+
+    it('able to edit form', () => {
+      const { getByTestId } = render(<CreateArticle/>)
+      const submitActive = getByTestId("submitForm");
+
+      expect(submitActive.contentEditable).toBe(true);
+    });
+
+    it('on submit function receive data', () => {
+      const { getByTestId } = render(<CreateArticle/>)
+      const submitActive = getByTestId("submitForm");
+
+      expect(submitActive.onsubmit.arguments).toBe(e);
+    });
+
+    it('submit inactive until form filled', () => {
+      const { getByTestId } = render(<CreateArticle/>)
+      const submitActive = getByTestId("submitForm");
+      const button = getByTestId("Button");
+       fireEvent.click(button)
+
+      expect(submitActive.onsubmit).toBe(false);
     });
--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -57,7 +57,7 @@ render(){
     return (<
         div >
         <
-        h2 className = 'sm:text-4xl text-2xl font-bold mt-6 text-gray-900' > Submit Article < /h2>    <
+        h2 className = 'submitForm' > Submit Article < /h2>    <
         p > This will be a form to allow submitters to submit an article
         for possible inclusion in our SEPER repo. < /p>
 
